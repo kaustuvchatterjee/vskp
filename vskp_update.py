1ece2318440a336e110d22e603e5a439cfa08792
kaustuvchatterjee <75204962+kaustuvchatterjee@users.noreply.github.com>
2022-07-28 10:45:56 +0530

    Add files via upload

--- vskp_update.py
+++ vskp_update.py
@@ -12,6 +12,19 @@ import re
 import pandas as pd
 from datetime import datetime, timedelta, date
 
+############
+#Procedures#
+############
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+#####################s
+
 # Read imd webpage for vskp
 url = "https://city.imd.gov.in/citywx/city_weather.php?id=43150"
 r = requests.get(url, verify=False)
@@ -32,65 +45,80 @@ for i in range(len(data)-1):
 df = pd.read_csv('vskp_data.csv')
 
 # Update maximum temperature
-i=0
-param = 'MaxTemp'
-str = cols[i]
-n=len(str)
-paramdate = datetime.strptime(str[n-9:n-1],'%d/%m/%y')
-value = values[i]
-print(param,paramdate,value)
-df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+try:
+    i=0
+    param = 'MaxTemp'
+    str = cols[i]
+    n=len(str)
+    paramdate = datetime.strptime(str[n-9:n-1],'%d/%m/%y')
+    value = values[i]
+    if is_number(value):
+        df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+except:
+    pass
 
 # Update minimum temperature
-i=2
-param = 'MinTemp'
-str = cols[i]
-n=len(str)
-paramdate = datetime.strptime(str[n-9:n-1],'%d/%m/%y')
-value = values[i]
-print(param,paramdate,value)
-df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+try:
+    i=2
+    param = 'MinTemp'
+    str = cols[i]
+    n=len(str)
+    paramdate = datetime.strptime(str[n-9:n-1],'%d/%m/%y')
+    value = values[i]
+    if is_number(value):
+        df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+except:
+    pass
+
 
 # Update precipitation
-i=4
-param = 'precip'
-str = cols[i]
-n=len(str)
-paramdate = date.today()-timedelta(days=1)
-value = values[i]
-print(param,paramdate,value)
-df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+try:
+    i=4
+    param = 'precip'
+    str = cols[i]
+    n=len(str)
+    paramdate = date.today()-timedelta(days=1)
+    value = values[i]
+    if is_number(value):
+        df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+except:
+    pass
 
 # Update morning relative humidity
-i=5
-param = 'RH'
-str = cols[i]
-n=len(str)
-pdate = date.today()
-pdate = pdate.strftime('%d/%m/%y')
-x = re.search('Humidity at\s',str)
-ptime = str[x.end():x.end()+4]
-pdate = pdate+":"+ptime
-paramdate = datetime.strptime(pdate,'%d/%m/%y:%H%M')
-value = values[i]
-print(param,paramdate,value)
-df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+try:
+    i=5
+    param = 'RH'
+    str = cols[i]
+    n=len(str)
+    pdate = date.today()
+    pdate = pdate.strftime('%d/%m/%y')
+    x = re.search('Humidity at\s',str)
+    ptime = str[x.end():x.end()+4]
+    pdate = pdate+":"+ptime
+    paramdate = datetime.strptime(pdate,'%d/%m/%y:%H%M')
+    value = values[i]
+    if is_number(value):
+        df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+except:
+    pass
 
 # Update morning relative humidity
-i=6
-param = 'RH'
-str = cols[i]
-n=len(str)
-pdate = date.today()
-pdate = pdate.strftime('%d/%m/%y')
-x = re.search('Humidity at\s',str)
-ptime = str[x.end():x.end()+4]
-pdate = pdate+":"+ptime
-paramdate = datetime.strptime(pdate,'%d/%m/%y:%H%M')
-value = values[i]
-print(param,paramdate,value)
-df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
-
+try:
+    i=6
+    param = 'RH'
+    str = cols[i]
+    n=len(str)
+    pdate = date.today()
+    pdate = pdate.strftime('%d/%m/%y')
+    x = re.search('Humidity at\s',str)
+    ptime = str[x.end():x.end()+4]
+    pdate = pdate+":"+ptime
+    paramdate = datetime.strptime(pdate,'%d/%m/%y:%H%M')
+    value = values[i]
+    if is_number(value):
+        df = df.append({'obsdate':paramdate, 'param':param, 'value':value}, ignore_index=True)
+except:
+    pass
 
 # Write csv file
 df.to_csv('vskp_data.csv',index=False)
